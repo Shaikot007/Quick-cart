d4168521683f627a83657708f8fe632f6d97196f
Shaikot007 <shaikot999@gmail.com>
2021-04-26 16:33:13 +0600

    Cart

--- src/Components/CartAndOrder/CartAndOrder.js
+++ src/Components/CartAndOrder/CartAndOrder.js
@@ -1,10 +1,61 @@
 import React from "react";
 import "./CartAndOrder.css";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import VisaCard from "../../Assets/Images/Visa_card.png";
+import MasterCard from "../../Assets/Images/Master_card.png";
+import Paypal from "../../Assets/Images/Paypal.png";
 
 function CartAndOrder() {
-  return(
+  return (
     <div className="CartAndOrder">
-      <h1>hello</h1>
+      <div className="CartAndOrderHeader">
+        <h1>Cart (0)</h1>
+      </div>
+      <div className="Cart">
+        <div className="CartItem">
+          <div className="CartItemHeader">
+            <p>Product</p>
+            <p>Title</p>
+            <p>Quantity</p>
+            <p>Price</p>
+          </div>
+          <div className="CartItemBox">
+            <p>Cart is empty</p>
+          </div>
+          <div className="CartItemFooter">
+            <div className="TotalBox">
+              <h6>Total:</h6>
+              <p>$ 0.00</p>
+            </div>
+            <div className="Payment">
+              <div className="PaymentOptions">
+                <img src={VisaCard} alt="Visa-card" />
+              </div>
+              <div className="PaymentOptions">
+                <img src={MasterCard} alt="Master-card" />
+              </div>
+              <div className="PaymentOptions">
+                <img src={Paypal} alt="Paypal" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="Checkout">
+          <Form>
+            <FormGroup>
+              <Label>Checkout</Label>
+              <Input type="text" name="text" placeholder="John Doe" />
+            </FormGroup>
+            <FormGroup>
+              <Input type="email" name="email" placeholder="quickcart@templete.buy" />
+            </FormGroup>
+            <FormGroup>
+              <Input type="tel" placeholder="01812345678" pattern="[0-9]{11}" />
+            </FormGroup>
+            <Button>COMPLETE THE ORDER</Button>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 };
